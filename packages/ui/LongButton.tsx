8c5f2899d165e5e7fe27ad095fe4ba7873551602
dannytszho <dannytszho@gmail.com>
2022-06-19 08:01:06 -0700

    Add beer, pool, dining svg

--- packages/ui/LongButton.tsx
+++ packages/ui/LongButton.tsx
@@ -1,5 +1,12 @@
+import BeerIcon from '../../apps/web/src/public/svg/BeerIcon'
 const LongButton = () => {
-  return <h2>Long Button</h2>
+  return (
+    <div className="w-[355px] h-[60px] border rounded-md">
+      <h2>
+        <BeerIcon />
+      </h2>
+    </div>
+  )
 }
 
 export default LongButton
