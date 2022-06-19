02919165633a37a283a19b215f23c1b67f8743f5
dannytszho <dannytszho@gmail.com>
2022-06-19 10:30:13 -0700

    Add uploadIcon SVG

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,17 +1,23 @@
-import { Buttonbb } from 'ui'
 import Image from 'next/image'
 import Nevada from '../public/Nevada.png'
-import LongButton from 'ui/LongButton'
+import { LongButton } from 'ui/Button'
+import { SquareButton } from 'ui/Button'
 import BeerIcon from '../public/svg/BeerIcon'
-import PoolBallIcon from '../public/svg/PoolBallIcon'
 import DiningIcon from '../public/svg/DiningIcon'
+import PoolBallIcon from '../public/svg/PoolBallIcon'
 import type { NextPage } from 'next'
+import UploadIcon from '../public/svg/UploadIcon'
 
 const Home: NextPage = () => {
   return (
     <>
       <div className="flex flex-col">
-        <div className="mt-40 m-12">
+        <div className="flex m-5 justify-end">
+          <SquareButton>
+            <UploadIcon />
+          </SquareButton>
+        </div>
+        <div className="mt-36 m-12">
           <Image src={Nevada} alt="main" />
         </div>
         <div className="flex flex-col gap-4 mt-20 m-5">
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,6 +1,3 @@
-import BeerIcon from '../../apps/web/src/public/svg/BeerIcon'
-import PoolBallIcon from '../../apps/web/src/public/svg/PoolBallIcon'
-import DiningIcon from '../../apps/web/src/public/svg/DiningIcon'
 import React from 'react'
 
 export const LongButton = ({ children }: { children: React.ReactNode }) => {
@@ -11,6 +8,10 @@ export const LongButton = ({ children }: { children: React.ReactNode }) => {
   )
 }
 
-export const SquareButton = () => {
-  return <div className="w-[50px] h-[50px] border rounded-lg bg-zinc-200"></div>
+export const SquareButton = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-[50px] h-[50px] border rounded-lg bg-zinc-200">
+      <h2>{children}</h2>
+    </div>
+  )
 }
