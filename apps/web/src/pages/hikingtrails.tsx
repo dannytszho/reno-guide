505a5648edb103bc84f43007411cdfc282219caf
dannytszho <dannytszho@gmail.com>
2022-06-23 15:20:24 -0700

    Add previous page routing on hikingtrails page

--- apps/web/src/pages/hikingtrails.tsx
+++ apps/web/src/pages/hikingtrails.tsx
@@ -1,17 +1,30 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { SquareButton } from 'ui'
+import LeftArrowIcon from '../public/svg/LeftArrowIcon'
 
 const hikingtrails = () => {
   return (
     <>
       <Head>
-        <title>🗻Trails - Reno & Lake Tahoe</title>
+        <title>🗻 Trails - Reno & Lake Tahoe</title>
         <meta name="description" content="Meta description for the Home page" />
       </Head>
-      <h2>hiking trails</h2>
-      <footer className="text-center text-xs">
-        <h3>&copy; 2022 Danny Tsui. All rights reserved.</h3>
-      </footer>
+      <div className="flex flex-col">
+        <div className="flex m-5 justify-start">
+          <SquareButton>
+            <Link href="/">
+              <div className="p-2.5">
+                <LeftArrowIcon />
+              </div>
+            </Link>
+          </SquareButton>
+        </div>
+        <h2>hiking trails</h2>
+        <footer className="text-center text-xs">
+          <h3>&copy; 2022 Danny Tsui. All rights reserved.</h3>
+        </footer>
+      </div>
     </>
   )
 }
