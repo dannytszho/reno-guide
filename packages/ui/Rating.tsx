671ab3779e18eb6d8db6a602b5a2be5d4283181c
dannytszho <dannytszho@gmail.com>
2022-06-26 10:43:19 -0700

    Add half star to Rating component

--- packages/ui/Rating.tsx
+++ packages/ui/Rating.tsx
@@ -1,15 +1,47 @@
 import { FaStar, FaStarHalfAlt } from 'react-icons/fa'
+// const Rating = ({ rating }: { rating: number }) => {
+//   const totalRating = 5
+
+//   return (
+//     <div className="flex relative">
+//       {[...Array(totalRating)].map((star, i) => {
+//         return (
+//           <>
+//             <div className="relative inline-flex">
+//               <div className="w-[100%] i overflow-hidden absolute">
+//                 <FaStar color={'#ffc107'} />
+//               </div>
+//               <div>
+//                 <FaStar color={'#e4e5e9'} />
+//               </div>
+//             </div>
+//           </>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+
 const Rating = ({ rating }: { rating: number }) => {
-  const totalRating = 5
   return (
-    <div className="flex relative">
-      {[...Array(totalRating)].map((star, i) => {
-        return i < rating ? (
-          <FaStar color={'#ffc107'} />
-        ) : (
-          <FaStar color={'#e4e5e9'} />
-        )
-      })}
+    <div>
+      <span className="flex">
+        {[...Array(Math.floor(rating))].map(key => (
+          <div key={key}>
+            <FaStar color={'#ffc107'} />
+          </div>
+        ))}
+        {rating - Math.floor(rating) && (
+          <div>
+            <FaStarHalfAlt color={'#ffc107'} />
+          </div>
+        )}
+        {[...Array(5 - Math.ceil(rating))].map(key => (
+          <div key={key}>
+            <FaStar color={'#e4e5e9'} />
+          </div>
+        ))}
+      </span>
     </div>
   )
 }
