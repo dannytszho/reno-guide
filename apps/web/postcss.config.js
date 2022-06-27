8370ac7d24457708c5dd095054d82c71c8ac6b78
dannytszho <dannytszho@gmail.com>
2022-06-27 15:19:00 -0700

    Configure postCSS with cssnano

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -2,18 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            // Added Sections Folder And Changed Extension To Jsx
-            Content: [
-              'src/**/*.{js,ts,jsx,tsx}',
-              '../../packages/ui/**/*.{js,ts,jsx,tsx}',
-              '../storybook/stories/**/*.{js,ts,jsx,tsx}',
-            ],
-            DefaultExtractor: Content => Content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 }
