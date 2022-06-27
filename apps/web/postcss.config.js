adf3202d21d1adde84b3f9c6aca03ed120aca664
dannytszho <dannytszho@gmail.com>
2022-06-27 15:08:42 -0700

    Configure postCSS

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
-  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            // Added Sections Folder And Changed Extension To Jsx
+            Content: [
+              'src/**/*.{js,ts,jsx,tsx}',
+              '../../packages/ui/**/*.{js,ts,jsx,tsx}',
+              '../storybook/stories/**/*.{js,ts,jsx,tsx}',
+            ],
+            DefaultExtractor: Content => Content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+  },
+}
