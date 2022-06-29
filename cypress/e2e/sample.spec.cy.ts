0530a01ca4d4adbc2badd14370af33640f627175
dannytszho <dannytszho@gmail.com>
2022-06-29 08:41:19 -0700

    Writing the first test in Cypress

---
+++ cypress/e2e/sample.spec.cy.ts
@@ -0,0 +1,14 @@
+// Testing RoadMap
+
+// Arrage - setup initail app this.state
+//   - vist a web page
+//   - query for an element
+// Act - take an action
+//   - interact with that element
+// Assert - make an assertion
+//   - make an assertion about page content
+describe('Go to reno-guide', () => {
+  it('should visit', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
