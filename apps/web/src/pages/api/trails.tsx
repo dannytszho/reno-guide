b01475d02d2a38291f8d88d1a354637bffa45f06
dannytszho <dannytszho@gmail.com>
2022-07-13 08:37:28 -0700

    Enable graphQL playground

--- apps/web/src/pages/api/trails.tsx
+++ apps/web/src/pages/api/trails.tsx
@@ -2,11 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { ApolloServer } from 'apollo-server-micro'
 import { typeDefs } from '../../../graphql/schema'
 import { resolvers } from '../../../graphql/resolvers'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import Cors from 'micro-cors'
 
 const cors = Cors()
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  typeDefs,
+  resolvers,
+})
 
 const startServer = apolloServer.start()
 
